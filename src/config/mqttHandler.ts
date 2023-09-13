4d1da4b9508421a137a99218a23fa2f2c9a30281
Roby Parlan <robbyparlan11@gmail.com>
2023-09-13 08:12:25 +0700

    process mqtt

--- src/config/mqttHandler.ts
+++ src/config/mqttHandler.ts
@@ -1,4 +1,5 @@
 import * as Mqtt from 'mqtt'
+import {db} from '../utils/util';
 import 'dotenv/config';
 
 var brokerUrl: any = process.env.MQTT_BROKER_URL
@@ -35,10 +36,42 @@ class MqttHandler {
     // mqtt subscriptions
 
     // When a message arrives, console.log it
-    this.mqttClient.on('message', function (topic: any, message: any) {
+    this.mqttClient.on('message', async function (topic: any, message: any) {
       console.log(message.toString());
       const jsonString = JSON.parse(message.toString());
-      console.log('jsonString : ',jsonString['uuid'])
+      console.log('jsonString : ',jsonString['data'])
+
+      let uuid = jsonString['uuid']
+      let project = jsonString['project']
+
+      let dataStream = jsonString['data'] && jsonString['data'].length > 0 ? jsonString['data'] : []
+      for (let i = 0; i < dataStream.length; i++) {
+        const el = dataStream[i];
+        let checkData = await db.select(db.raw(`*`)).from('watermonitoringv2').whereRaw(`uuid = ? AND project = ? AND time = ?`, [uuid, project, el['time']])
+
+        if (checkData.length === 0) {
+          await db('watermonitoringv2')
+          .insert({
+            uuid: uuid,
+            project: project,
+            time: el['time'],
+            temperature: el['Tempertature'],
+            do_: el['DO'],
+            tur: el['TUR'],
+            ct: el['CT'],
+            ph: el['pH'],
+            orp: el['ORP'],
+            bod: el['BOD'],
+            cod: el['COD'],
+            tss: el['TSS'],
+            n: el['N'],
+            no3_n: el['NO3_N'],
+            no2: el['NO2'],
+            depeth: el['Depeth'],
+          })
+        }
+
+      }
 
       //process insert to DB
     });
