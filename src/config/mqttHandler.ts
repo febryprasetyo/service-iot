185c935085390ff9b61d5557945ace964c0c734a
Roby Parlan <robbyparlan11@gmail.com>
2024-10-30 18:34:39 +0700

    fixing issue transaction timeout

--- src/config/mqttHandler.ts
+++ src/config/mqttHandler.ts
@@ -47,54 +47,64 @@ class MqttHandler {
 
       let dataStream = jsonString['data'] && jsonString['data'].length > 0 ? jsonString['data'] : []
       for (let i = 0; i < dataStream.length; i++) {
-        const el = dataStream[i];
-        // let checkData = await db.select(db.raw(`*`)).from('mqtt_datas').whereRaw(`uuid = ? AND project = ? AND time = ?`, [uuid, project, el['time']])
-        let tm = el['time'] ? moment(el['time'],  "DD-MM-YYYY HH:ss:mm").format('YYYY-MM-DD HH:ss:mm') : moment().format('YYYY-MM-DD HH:ss:mm')
-        console.log(`----------------------------- tm : `, tm)
-        let checkData = await db.select(db.raw(`*`)).from('mqtt_datas').whereRaw(`uuid = ? AND time = ?`, [uuid, tm])
-
-        if (checkData.length === 0) {
-          await db('mqtt_datas')
-          .insert({
-            uuid: uuid,
-            time: tm,
-            temperature: el['Temperature'].toFixed(2),
-            do_: el['DO'].toFixed(2),
-            tur: el['TUR'].toFixed(2),
-            ph: el['PH'].toFixed(2),
-            bod: el['BOD'].toFixed(2),
-            cod: el['COD'].toFixed(2),
-            tss: el['TSS'].toFixed(2),
-            depth: el['DEPTH'].toFixed(2),
-            no3_3: el['NO3-3'].toFixed(2),
-            n: el['N'].toFixed(2),
-            ct: el['CT'].toFixed(2),
-            no2: el['NO2'].toFixed(2),
-            orp: el['ORP'].toFixed(2),
-            'lgnh4+': el['LgNH4+'],
-            liquid: el['Liquid'],
-          })
-
-          let checkDataStasiun = await db.select(db.raw(`*`)).from('devices').whereRaw(`id_mesin = ?`, uuid)
-
-          await db('watermonitoring')
+        let trx
+        try {
+          const el = dataStream[i];
+          // let checkData = await db.select(db.raw(`*`)).from('mqtt_datas').whereRaw(`uuid = ? AND project = ? AND time = ?`, [uuid, project, el['time']])
+          let tm = el['time'] ? moment(el['time'],  "DD-MM-YYYY HH:ss:mm").format('YYYY-MM-DD HH:ss:mm') : moment().format('YYYY-MM-DD HH:ss:mm')
+          console.log(`----------------------------- tm : `, tm)
+          trx = await db.transaction()
+          let checkData = await trx.select(trx.raw(`*`)).from('mqtt_datas').whereRaw(`uuid = ? AND time = ?`, [uuid, tm])
+  
+          if (checkData.length === 0) {
+            await trx('mqtt_datas')
             .insert({
               uuid: uuid,
-              id_stasiun: checkDataStasiun.length > 0 ? checkDataStasiun[0].nama_stasiun : '-',
               time: tm,
               temperature: el['Temperature'].toFixed(2),
               do_: el['DO'].toFixed(2),
-              turbidity: el['TUR'].toFixed(2),
+              tur: el['TUR'].toFixed(2),
               ph: el['PH'].toFixed(2),
               bod: el['BOD'].toFixed(2),
               cod: el['COD'].toFixed(2),
               tss: el['TSS'].toFixed(2),
-              waterlevel: el['DEPTH'].toFixed(2),
-              no3: el['NO3-3'].toFixed(2),
-              nh3n: el['N'].toFixed(2),
-              tds: el['CT'].toFixed(2),
+              depth: el['DEPTH'].toFixed(2),
+              no3_3: el['NO3-3'].toFixed(2),
+              n: el['N'].toFixed(2),
+              ct: el['CT'].toFixed(2),
+              no2: el['NO2'].toFixed(2),
+              orp: el['ORP'].toFixed(2),
+              'lgnh4+': el['LgNH4+'],
+              liquid: el['Liquid'],
             })
-        }
+  
+            let checkDataStasiun = await trx.select(trx.raw(`*`)).from('devices').whereRaw(`id_mesin = ?`, uuid)
+  
+            await trx('watermonitoring')
+              .insert({
+                uuid: uuid,
+                id_stasiun: checkDataStasiun.length > 0 ? checkDataStasiun[0].nama_stasiun : '-',
+                time: tm,
+                temperature: el['Temperature'].toFixed(2),
+                do_: el['DO'].toFixed(2),
+                turbidity: el['TUR'].toFixed(2),
+                ph: el['PH'].toFixed(2),
+                bod: el['BOD'].toFixed(2),
+                cod: el['COD'].toFixed(2),
+                tss: el['TSS'].toFixed(2),
+                waterlevel: el['DEPTH'].toFixed(2),
+                no3: el['NO3-3'].toFixed(2),
+                nh3n: el['N'].toFixed(2),
+                tds: el['CT'].toFixed(2),
+              })
+          }
+
+          await trx.commit()
+          
+        } catch (error) {
+          if (trx) trx.rollback()
+          console.log(`----------------------------- error mqtt data : `, error)
+        } 
 
       }
 
