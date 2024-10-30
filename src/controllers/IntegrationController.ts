2a7697aec073287ace6f3d5465e352c96fcb664b
Roby Parlan <robbyparlan11@gmail.com>
2024-10-30 20:57:17 +0700

    modified api klhk

--- src/controllers/IntegrationController.ts
+++ src/controllers/IntegrationController.ts
@@ -102,129 +102,134 @@ class IntegrationController {
             logger.info(`------------------- SYNC DATA IS EMPTY ------------------`)
             continue
           }
-    
-          let dataIot: any = {
-            temperature: 0,
-            tds: 0,
-            do_: 0,
-            ph: 0,
-            turbidity: 0,
-            waterlevel: 0,
-            'no3': 0,
-            'nh3n': 0,
-            cod: 0,
-            bod: 0,
-            tss: 0,
-          }
+
           let tmpData: any = []
-          let count: any = 0
-    
-          for (let i = 0; i < data.length; i++) {
-            const el: any = data[i];
-            logger.info(`------------------- SYNC DATA PROCESSING ${i+1}/${data.length} ------------------`)
-            try {
-              tmpData.push(el.id)
-              count += 1
-              dataIot.id_stasiun = el.id_stasiun
-              dataIot.temperature += parseFloat(el.temperature)
-              dataIot.tds += parseFloat(el.ct)
-              dataIot.do_ += parseFloat(el.do_)
-              dataIot.ph += parseFloat(el.ph)
-              dataIot.turbidity += parseFloat(el.tur)
-              dataIot.waterlevel += parseFloat(el.depth)
-              dataIot['no3'] += parseFloat(el['no3_3'])
-              dataIot['nh3n'] += parseFloat(el['n'])
-              dataIot.cod += parseFloat(el.cod)
-              dataIot.bod += parseFloat(el.bod)
-              dataIot.tss += parseFloat(el.tss)
-    
-            } catch (error: any) {
-              if (!errorCodes[error.code])
-                logger.error(error)
-            }        
+          for (let t = 0; t < data.length; t++) {
+            const eld = data[t];
+            tmpData.push(eld.id)
           }
-    
-          let options = {
-            url: process.env.URL_KLHK,
-            method: 'POST',
-            header: {
-              'Content-Type': 'Application/json'
-            },
-            data: {
-              "data" : {
-                "IDStasiun" : dataIot.id_stasiun,
-                "Tanggal" : moment().format(`YYYY-MM-DD`),
-                "Jam" : moment().format(`HH:ss:mm`),
-                "Suhu" : (parseFloat(dataIot.temperature) / count).toFixed(2),
-                // "DHL" : 0,
-                "TDS" : (parseFloat(dataIot.tds) / count).toFixed(2),
-                // "Salinitas" : 0,
-                "DO" : (parseFloat(dataIot.do_) / count).toFixed(2),
-                "PH" : (parseFloat(dataIot.ph) / count).toFixed(2),
-                "Turbidity" : (parseFloat(dataIot.turbidity) / count).toFixed(2),
-                "Kedalaman" : (parseFloat(dataIot.waterlevel) / count).toFixed(2),
-                // "SwSG" : 0,
-                "Nitrat" : (parseFloat(dataIot['no3']) / count).toFixed(2),
-                "Amonia" : (parseFloat(dataIot['nh3n']) / count).toFixed(2),
-                // "ORP" : 0,
-                "COD" : (parseFloat(dataIot.cod) / count).toFixed(2),
-                "BOD" : (parseFloat(dataIot.bod) / count).toFixed(2),
-                "TSS" : (parseFloat(dataIot.tss) / count).toFixed(2)
-                },
-                "apikey" : data[0].api_key,
-                "apisecret" : data[0].secret_key
+          let ctx: any = Object.values(
+            data.reduce((acc: any, item: any) => {
+              if (!acc[item.id_stasiun]) {
+                acc[item.id_stasiun] = {
+                  id_stasiun: item.id_stasiun,
+                  temperature: 0,
+                  tds: 0,
+                  do_: 0,
+                  ph: 0,
+                  turbidity: 0,
+                  waterlevel: 0,
+                  'no3': 0,
+                  'nh3n': 0,
+                  cod: 0,
+                  bod: 0,
+                  tss: 0,
+                  count: 0,
+                }
               }
-          }
+              acc[item.id_stasiun].count += 1
+              acc[item.id_stasiun].temperature += parseFloat(item.temperature)
+              acc[item.id_stasiun].tds += parseFloat(item.ct)
+              acc[item.id_stasiun].do_ += parseFloat(item.do_)
+              acc[item.id_stasiun].ph += parseFloat(item.ph)
+              acc[item.id_stasiun].turbidity += parseFloat(item.tur)
+              acc[item.id_stasiun].waterlevel += parseFloat(item.depth)
+              acc[item.id_stasiun]['no3'] += parseFloat(item['no3_3'])
+              acc[item.id_stasiun]['nh3n'] += parseFloat(item['n'])
+              acc[item.id_stasiun].cod += parseFloat(item.cod)
+              acc[item.id_stasiun].bod += parseFloat(item.bod)
+              acc[item.id_stasiun].tss += parseFloat(item.tss)
+              return acc;
+            }, {})
+          );
+
+          console.log(`------------------------------- ctx : `, ctx)
     
-          logger.info(`------------------- SYNC SUBMIT DATA TO API MENLHK ------------------`)
-          let result = await axios.request(options)
-          logger.info(`------------------- SYNC RESPONSE FROM API MENLHK ${JSON.stringify(result.data)} ------------------`)
-          let statusCode = result.data.status ? result.data.status.statusCode : 401
-    
-          if (statusCode == 200) {
-            logger.info(`------------------- [SYNC-SUCCESS] UPDATE DATA WATERMONITORING ------------------`)
-  
-            for (let r = 0; r < tmpData.length; r++) {
-              const el = tmpData[r];
-              await db.table('mqtt_datas')
-              .whereRaw(`id = ?`, el)
-              .update({
-                is_success: true,
-                sync_time: new Date(),
-                // res_menlhk: JSON.stringify({req: options, res:result.data})
-              })
+          for (let z = 0; z < ctx.length; z++) {
+            const elz: any = ctx[z];
+            
+            let options = {
+              url: process.env.URL_KLHK,
+              method: 'POST',
+              header: {
+                'Content-Type': 'Application/json'
+              },
+              data: {
+                "data" : {
+                  "IDStasiun" : elz.id_stasiun,
+                  "Tanggal" : moment().format(`YYYY-MM-DD`),
+                  "Jam" : moment().format(`HH:ss:mm`),
+                  "Suhu" : (parseFloat(elz.temperature) / elz.count).toFixed(2),
+                  // "DHL" : 0,
+                  "TDS" : (parseFloat(elz.tds) / elz.count).toFixed(2),
+                  // "Salinitas" : 0,
+                  "DO" : (parseFloat(elz.do_) / elz.count).toFixed(2),
+                  "PH" : (parseFloat(elz.ph) / elz.count).toFixed(2),
+                  "Turbidity" : (parseFloat(elz.turbidity) / elz.count).toFixed(2),
+                  "Kedalaman" : (parseFloat(elz.waterlevel) / elz.count).toFixed(2),
+                  // "SwSG" : 0,
+                  "Nitrat" : (parseFloat(elz['no3']) / elz.count).toFixed(2),
+                  "Amonia" : (parseFloat(elz['nh3n']) / elz.count).toFixed(2),
+                  // "ORP" : 0,
+                  "COD" : (parseFloat(elz.cod) / elz.count).toFixed(2),
+                  "BOD" : (parseFloat(elz.bod) / elz.count).toFixed(2),
+                  "TSS" : (parseFloat(elz.tss) / elz.count).toFixed(2)
+                  },
+                  "apikey" : data[0].api_key,
+                  "apisecret" : data[0].secret_key
+                }
             }
-            await db('res_klhk')
+      
+            logger.info(`------------------- SYNC SUBMIT DATA TO API MENLHK ------------------`)
+            let result = await axios.request(options)
+            logger.info(`------------------- SYNC RESPONSE FROM API MENLHK ${JSON.stringify(result.data)} ------------------`)
+            let statusCode = result.data.status ? result.data.status.statusCode : 401
+      
+            if (statusCode == 200) {
+              logger.info(`------------------- [SYNC-SUCCESS] UPDATE DATA WATERMONITORING ------------------`)
+    
+              for (let r = 0; r < tmpData.length; r++) {
+                const el = tmpData[r];
+                await db.table('mqtt_datas')
+                .whereRaw(`id = ?`, el)
+                .update({
+                  is_success: true,
+                  sync_time: new Date(),
+                  // res_menlhk: JSON.stringify({req: options, res:result.data})
+                })
+              }
+              await db('res_klhk')
+                .insert({
+                  payload: JSON.stringify(options.data),
+                  data_uid: result.data.rows.data_uid,
+                  status_code: result.data.status.statusCode,
+                  status_desc: result.data.status.statusDesc,
+                  id_stasiun: options.data.data['IDStasiun']
+                })
+    
+              logger.info(`------------------- [SYNC-SUCCESS] UPDATE SUCCESFULLY ------------------`)
+            } else {
+              logger.info(`------------------- [SYNC-FAILED] UPDATE DATA WATERMONITORING ------------------`)
+              for (let r = 0; r < tmpData.length; r++) {
+                const el = tmpData[r];
+                await db.table('mqtt_datas')
+                .whereRaw(`id = ?`, el)
+                .update({
+                  is_success: true,
+                  sync_time: new Date(),
+                  // res_menlhk: JSON.stringify({req: options, res:result.data})
+                })
+              }
+              await db('res_klhk')
               .insert({
                 payload: JSON.stringify(options.data),
-                data_uid: result.data.rows.data_uid,
-                status_code: result.data.status.statusCode,
-                status_desc: result.data.status.statusDesc,
+                data_uid: '-',
+                status_code: null,
+                status_desc: result.data,
                 id_stasiun: options.data.data['IDStasiun']
               })
-  
-            logger.info(`------------------- [SYNC-SUCCESS] UPDATE SUCCESFULLY ------------------`)
-          } else {
-            logger.info(`------------------- [SYNC-FAILED] UPDATE DATA WATERMONITORING ------------------`)
-            for (let r = 0; r < tmpData.length; r++) {
-              const el = tmpData[r];
-              await db.table('mqtt_datas')
-              .whereRaw(`id = ?`, el)
-              .update({
-                is_success: true,
-                sync_time: new Date(),
-                // res_menlhk: JSON.stringify({req: options, res:result.data})
-              })
+              logger.info(`------------------- [SYNC-FAILED] UPDATE SUCCESFULLY ------------------`)
             }
-            await db('res_klhk')
-            .insert({
-              payload: JSON.stringify(options.data),
-              data_uid: '-',
-              status_code: null,
-              status_desc: result.data,
-              id_stasiun: options.data.data['IDStasiun']
-            })
-            logger.info(`------------------- [SYNC-FAILED] UPDATE SUCCESFULLY ------------------`)
           }
         }
   
